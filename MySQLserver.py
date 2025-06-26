012e2bd27e50024e95c3ef26f2b99604cf447d2c
muekep <63904395+muekep@users.noreply.github.com>
2025-06-26 15:44:52 +0300

    py in sql

---
+++ MySQLserver.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from mysql.connector import errorcode
+def create_database():  
+  try: 
+    alx_book_store = mysql.connector.connect(
+      host = "lcoalhost",
+      user = "username",
+      password = "today",
+      database = "alx_book_store"
+    )
+    cursor = alx_book_store.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+  execpt mysql.connector.Error as err:
+    print(f"Error: {err}")
+finally:
+  if alx_book_store.is_connected():
+    cursor.close()
+    alx_book_store.close()
+  
