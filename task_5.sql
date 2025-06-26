91ef0cefc24336cf4669a7624eb41a5cf3406824
muekep <63904395+muekep@users.noreply.github.com>
2025-06-26 16:45:04 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -13,6 +13,6 @@ cursor = alx_book_store.cursor()
   
 cursor.execute("""
   USE alx_book_store;
-  INSERT INTO Customers (customer_id, customer_name, email, address)
+  INSERT INTO customer (customer_id, customer_name, email, address)
   VALUES(1,"Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave")
 """)
