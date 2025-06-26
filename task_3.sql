192b3fa1a6f19949f4ddc764ae9167db64249266
muekep <63904395+muekep@users.noreply.github.com>
2025-06-26 16:23:43 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,2 +1,16 @@
-USE alx_book_store;
-SHOW TABLES;
+import mysql.connector
+from mysql.connector import Error
+
+
+# Connect to the alx_book_store database
+alx_book_store = mysql.connector.connect(
+  host = "lcoalhost",
+  user = "username",
+  password = "today",
+  database = "alx_book_store"
+)
+cursor = alx_book_store.cursor()
+  
+cursor.execute("""
+  SHOW TABLES
+""")
