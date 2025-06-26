67064693a6ee6e629fa536a2952737b3d4ad70d3
muekep <63904395+muekep@users.noreply.github.com>
2025-06-26 15:25:08 +0300

    SQL

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -39,7 +39,7 @@ CREATE TABLE Orders (
 
 CREATE TABLE Order_Details (
     orderdetailid VARCHAR(50) PRIMARY KEY,
-    order_id VARCHAR(50) REFERENCES Orders(order_id),
-    book_id VARCHAR(50) REFERENCES Books(book_id),
+    order_id INT FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
